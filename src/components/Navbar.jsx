a0edc106f111647631cec648dc7f4a68800efd44
Rajveer* <rajveer.24bcs10404@sst.scaler.com>
2025-07-27 02:50:10 +0530

    Fix: Added glassy effect to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
-        <nav className='flex space-x-8 items-center p-4 bg-black'>
+        <nav className='sticky top-0 z-50 flex space-x-8 items-center p-4 bg-transparent/10 backdrop-blur-md border border-white/20 rounded-xl shadow-lg transition-all duration-300 ease-in-out hover:bg-white/20'>
             <img src='/logo.svg' alt="logo" className='w-[50px]' />
             <NavLink to={""} className='text-3xl font-bold text-blue-500'>Anime</NavLink>
             <NavLink to={"/watchlist"} className='text-3xl font-bold text-blue-500'>Watch-list</NavLink>
